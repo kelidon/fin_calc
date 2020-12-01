f25c6bcf58abe81f7d389a3471483ca0e188beb5
kelidon <v.shchors@gmail.com>
2020-12-02 00:16:53 +0300

    format rounded value

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -150,7 +150,7 @@ class _HomePageState extends State<HomePage> {
                                               )
                                             ],
                                           ),
-                                          OutputWidget(text: roundedResult),
+                                          OutputWidget(text: StringFormatter.beautifyOutput(roundedResult)),
                                         ],
                                       );
                                     },
